31fbecffce761765b053af8e6d34e197c740e08c
Gabriel F <gabpaivapereira@gmail.com>
2021-08-09 00:56:13 -0300

    first version

---
+++ src/player.py
@@ -0,0 +1,61 @@
+import pygame
+from laser import Laser
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, constraint, speed):
+        super().__init__()
+        self.image = pygame.image.load('../graphics/player.png').convert_alpha()
+        self.size = self.image.get_size()
+
+        proportion = 1
+        self.image = self.scale_image(self.image,proportion)
+        self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = 5
+        self.max_x_constraint = constraint
+        self.ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 600
+
+        self.lasers = pygame.sprite.Group()
+
+        self.laser_sound = pygame.mixer.Sound('../audio/audio_laser.wav')
+        self.laser_sound.set_volume(0.09)
+
+    def scale_image(self, image, proportion):
+        #scaling image
+        return pygame.transform.scale(image, (int(self.size[0] * proportion),(int(self.size[1] * proportion))))
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+
+        if keys[pygame.K_SPACE] and self.ready:
+            self.shoot_laser()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks()
+            self.laser_sound.play()
+
+    def recharge(self):
+        if not self.ready:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_time >= self.laser_cooldown:
+                self.ready = True
+
+    def constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
+
+    def shoot_laser(self):
+        self.lasers.add(Laser(self.rect.center,-8,self.rect.bottom))
+
+    def update(self):
+        self.get_input()
+        self.constraint()
+        self.recharge()
+        self.lasers.update()
